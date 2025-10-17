902a322c2b16f4e17442e13e7f75b6f112e01ae1
muhammad sarim <muhammadsarim1861@gmail.com>
2025-10-17 21:05:58 +0500

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+// function checkLength(){
+//     let text = document.getElementById("input").value
+//     let length = text.length
+//     document.getElementById("result").innerText = "You typed " + length + " characters"
+// }
+
+
+
+function checkLength (){
+    let text = document.getElementById("input").value
+    let length = text.length
+    document.getElementById("result").innerText = "You tyed " + length + " characters"
+}
